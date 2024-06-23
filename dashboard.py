1e825cfbd8efc6d6d43ff8497368ea55cc67f7ef
dyahtri <dyahttari@gmail.com>
2024-06-23 21:17:30 +0700

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -17,7 +17,7 @@ def get_model(model_name):
     if model_name == "Random Forest":
         return RandomForestClassifier(n_estimators=100, max_depth=5, n_jobs=-1)
     else:
-        return DecisionTreeClassifier(max_depth=10) 
+        return DecisionTreeClassifier(max_depth=3) 
 
 def train_model(model, X_train, y_train):
     model.fit(X_train, y_train)
